b8d206ee17f1bdca3b04a5fa7524f1b5703334f4
ht7200 <251741906@qq.com>
2016-06-14 08:40:29 +0800

    QAQ

---
+++ js/config.js
@@ -0,0 +1,12 @@
+
+//var ip = "192.168.100.70:7050";//�ĳ�IP
+var ip ="192.168.0.150:8055";
+
+var ipNJ="192.168.100.70:7050";
+
+var Zyip ="192.168.0.150:7050";//�ĳ�IP
+
+var lip = "localhost:6959";
+
+//var sys_url = "http://" + ip + "/AppSys.asmx/Login";
+var CreateQst_url = "http://" + ip + "/AppSys.asmx/CreateQst";
